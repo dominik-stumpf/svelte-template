f4878621804dcfe08f79e075ca69408e444bc429
Dominik Stumpf <s.dominik.work@gmail.com>
2025-01-06 00:40:19 +0100

    revert: undo unocss

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import "virtual:uno.css";
-  import "../styles/global.css";
-  let { children } = $props();
-</script>
-
-{@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import extractorSvelte from '@unocss/extractor-svelte'
-import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-	plugins: [
-    UnoCSS({
-      extractors: [
-        extractorSvelte(),
-      ],
-      /* more options */
-    }),
-    sveltekit()
-  ]
+	plugins: [sveltekit()]
 });
