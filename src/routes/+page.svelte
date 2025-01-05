880042e421f24aedc2e549b8cdecde735d239a85
Dominik Stumpf <s.dominik.work@gmail.com>
2025-01-05 23:29:02 +0100

    feat: add unocss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to <span class="text-red-500">SvelteKit</span></h1>
+<p>
+  Visit <a class="dark:outline" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+<div class="font-mono text-lg">mono font</div>
+<div class="font-sans text-lg">sans font</div>
+<div class="i-ph-anchor-simple-thin"></div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import extractorSvelte from '@unocss/extractor-svelte'
+import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+    UnoCSS({
+      extractors: [
+        extractorSvelte(),
+      ],
+      /* more options */
+    }),
+    sveltekit()
+  ]
 });
