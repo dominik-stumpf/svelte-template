6d4269c0ab0fb4ae503e0c2d448e3c12a3939a97
Dominik Stumpf <s.dominik.work@gmail.com>
2025-01-06 01:29:51 +0100

    feat: add iconify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button/index";
+  import Icon from "@iconify/svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <Button>button</Button>
+<Icon icon="ph:airplane-landing-bold" width="256" height="256" />
