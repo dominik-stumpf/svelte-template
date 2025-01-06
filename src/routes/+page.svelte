5748afd0a86e094794779b2eb01f3a236dd4b50e
Dominik Stumpf <s.dominik.work@gmail.com>
2025-01-06 01:53:51 +0100

    chore: add tw vite plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   import Icon from "@iconify/svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to <span class="text-red-500 dark:outline">SvelteKit</span></h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <Button>button</Button>
 <Icon icon="ph:airplane-landing-bold" width="256" height="256" />
